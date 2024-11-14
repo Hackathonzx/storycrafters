9d39e5ff43c5de2636c46fe8d4ce941748956484
Jumcee <futhmah456@gmail.com>
2024-11-14 14:59:02 +0100

    story

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,10 +5,10 @@ require("dotenv").config();
 const { RPC_URL, PRIVATE_KEY } = process.env
 
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.1",
   networks: {
-    WorldChainSepoliaTestnet: {
-      url: process.env.AVALANCHE_URL,
+    TaikoHeklaL2: {
+      url: process.env.RPC_URL,
       chainId: 167009,
       accounts: [process.env.PRIVATE_KEY],
     },
