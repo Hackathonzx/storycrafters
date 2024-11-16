beacba0981d0668bdd355bfb25a9eb99026ac814
Jumcee <futhmah456@gmail.com>
2024-11-16 18:22:11 +0100

    storycrafters

--- ignition/modules/deploy.js
+++ ignition/modules/deploy.js
@@ -1,23 +1,23 @@
 const hre = require("hardhat");
 
 async function main() {
-  // // Deploy StoryNFT contract
-  // const StoryNFT = await hre.ethers.getContractFactory("StoryNFT");
-  // const storyNFT = await StoryNFT.deploy();
-  // await storyNFT.waitForDeployment();
-  // console.log("StoryNFT deployed to:", await storyNFT.getAddress());
+  // Deploy StoryNFT contract
+  const StoryNFT = await hre.ethers.getContractFactory("StoryNFT");
+  const storyNFT = await StoryNFT.deploy();
+  await storyNFT.waitForDeployment();
+  console.log("StoryNFT deployed to:", await storyNFT.getAddress());
 
-  // // Deploy Voting contract
-  // const Voting = await hre.ethers.getContractFactory("Voting");
-  // const voting = await Voting.deploy();
-  // await voting.waitForDeployment();
-  // console.log("Voting deployed to:", await voting.getAddress());
+  // Deploy Voting contract
+  const Voting = await hre.ethers.getContractFactory("Voting");
+  const voting = await Voting.deploy();
+  await voting.waitForDeployment();
+  console.log("Voting deployed to:", await voting.getAddress());
 
-  // // Deploy GuildSystem contract
-  // const GuildSystem = await hre.ethers.getContractFactory("GuildSystem");
-  // const guildSystem = await GuildSystem.deploy();
-  // await guildSystem.waitForDeployment();
-  // console.log("GuildSystem deployed to:", await guildSystem.getAddress());
+  // Deploy GuildSystem contract
+  const GuildSystem = await hre.ethers.getContractFactory("GuildSystem");
+  const guildSystem = await GuildSystem.deploy();
+  await guildSystem.waitForDeployment();
+  console.log("GuildSystem deployed to:", await guildSystem.getAddress());
 
   // Deploy NFTMarketplace contract
   const NFTMarketplace = await hre.ethers.getContractFactory("NFTMarketplace");
